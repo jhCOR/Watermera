de9a9ddf5f61e56af552a85e3a8a8e890565d158
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-04-11 14:54:47 +0000

    Added checkToken middleware

--- backend/src/responses/responses/ErrorResponse.ts
+++ backend/src/responses/responses/ErrorResponse.ts
@@ -2,7 +2,7 @@ import ResponseBase from "../ResponseBase";
 
 export enum ReqError{
 	MissingProp = 1, //Request is incomplete
-	Unauthenticated, //User's token is invalid
+	InvalidToken, //User's token is invalid
 	NoToken, //Token is missing
 }
 
